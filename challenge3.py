d6ec60c63ca47772fc5e7d8045177d3c20643c6c
bpulcini <bethanypulcini@gmail.com>
2018-05-12 17:57:37 -0700

    Cleaned up challenge 3

--- challenge3.py
+++ challenge3.py
@@ -1,14 +1,21 @@
 import binascii
 
 hex_encoded_str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-unhexedstr = binascii.unhexlify(hex_encoded_str)
-
-all_results = []
-for i in range(256):
-    holder = []
-    for x in unhexedstr:
-        holder.append(chr(i ^ ord(x)))
-    all_results.append(''.join([str(e) for e in holder]))
 
 
-print max(all_results, key=lambda s: s.count(' '))
+def single_byte_xor(hex_string, key):
+    unhexed = binascii.unhexlify(hex_string)
+    str_holder = []
+    for x in unhexed:
+        str_holder.append(chr(key ^ ord(x)))
+    return ''.join([str(e) for e in str_holder])
+
+if __name__ == '__main__':
+    all_key_tries = []
+    for i in range(256):
+        result = single_byte_xor(hex_encoded_str, i)
+        all_key_tries.append([result, i])
+
+    deciphered_text, key = max(all_key_tries, key=lambda s: s[0].count(' '))
+
+    print "Deciphered string with key " + str(hex(key)) + " (ascii character " + chr(key) + ") and got messsage " + deciphered_text
