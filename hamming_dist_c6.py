b81b2de453c1c148ae66f77c6ffee6f2413ab5ac
bpulcini <bethanypulcini@gmail.com>
2018-05-13 11:45:17 -0700

    file/function to compute hamming distance

---
+++ hamming_dist_c6.py
@@ -0,0 +1,27 @@
+string1 = "this is a test"
+string2 = "wokka wokka!!!"
+
+len_string = max([len(string1), len(string2)])
+
+bin_1_a = ''.join([format(ord(c), 'b').zfill(8) for c in string1])
+bin_2_a = ''.join([format(ord(c), 'b').zfill(8) for c in string2])
+
+
+def get_hamming_dist(bin_1, bin_2):
+    if len(bin_1) > len(bin_2):
+        bin_2.zfill(len(bin_1))
+        print "length of binary data not the same"
+    elif len(bin_2) > len(bin_1):
+        bin_1.zfill(len(bin_2))
+        print "length of binary data not the same"
+
+    hamming_dist = 0
+    for i in range(len(bin_1)):
+        if bin_1[i] != bin_2[i]:
+            hamming_dist += 1
+
+    return hamming_dist
+
+
+if __name__ == "__main__":
+    print get_hamming_dist(bin_1_a, bin_2_a)
