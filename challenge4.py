9f632abf62235ee2e8ce4233b1d0825dbc01f9c2
bpulcini <bethanypulcini@gmail.com>
2018-05-12 17:57:56 -0700

    Completed challenge 4

---
+++ challenge4.py
@@ -0,0 +1,21 @@
+from challenge3 import single_byte_xor
+import string
+
+filename = 'challenge4data.txt'
+
+with open(filename, 'r') as f:
+    all_line_results = []
+    for line_num, line in enumerate(f):
+        line_key_results = []
+        for i in range(256):
+            line_key_results.append([single_byte_xor(line.rstrip(), i), line_num, i])
+
+        best_guess = max(line_key_results, key=lambda s: s[0].count(' '))
+
+        un_print = [x for x in best_guess[0] if x not in string.printable]
+        #
+        if not un_print:
+            all_line_results.append(best_guess)
+
+    for x in all_line_results:
+        print "Deciphered line " + str(x[1]) + " with key " + str(hex(x[2])) + " (ascii character " + chr(x[2]) + ") and got messsage: \"" + str(x[0]).rstrip() + "\" "
