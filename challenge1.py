a7b790bebe361a0888443bc8d1bd9dfe72e0a31c
bpulcini <bethanypulcini@gmail.com>
2018-08-12 19:59:59 -0700

    Updated challenge 1 for python 3

--- challenge1.py
+++ challenge1.py
@@ -1,11 +1,10 @@
 import binascii
 
+def hex_to_b64(hex_string):
+    return binascii.b2a_base64(binascii.unhexlify(hex_string), newline=False).decode('ascii')
+
+
 hexed_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
-
-def hex_to_b64(input):
-    return binascii.b2a_base64(binascii.unhexlify(input)).rstrip()
-
-
-print hex_to_b64(hexed_string) == expected
+print(hex_to_b64(hexed_string) == expected)
