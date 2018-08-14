2bc5e47b723ea9e8b11cf50a16881a74a1b70914
bpulcini <bethanypulcini@gmail.com>
2018-08-13 19:04:54 -0700

    updated 4 for python 3

--- challenge4.py
+++ challenge4.py
@@ -8,14 +8,18 @@ with open(filename, 'r') as f:
     for line_num, line in enumerate(f):
         line_key_results = []
         for i in range(256):
-            line_key_results.append([single_byte_xor(line.rstrip(), i), line_num, i])
+            line_key_results.append([single_byte_xor(line.rstrip(), i), line_num, i, line])
 
         best_guess = max(line_key_results, key=lambda s: s[0].count(' '))
 
         un_print = [x for x in best_guess[0] if x not in string.printable]
-        #
+
         if not un_print:
             all_line_results.append(best_guess)
 
+    # print(all_line_results)
+
     for x in all_line_results:
-        print "Deciphered line " + str(x[1]) + " with key " + str(hex(x[2])) + " (ascii character " + chr(x[2]) + ") and got messsage: \"" + str(x[0]).rstrip() + "\" "
+        print("Deciphered line " + str(x[1]) + " with key " + str(hex(x[2])) + " (ascii character " + chr(x[2]) + ")")
+        print("Original line: \"" + str(x[3]).rstrip() + "\"")
+        print("Deciphered message: \"" + str(x[0]).rstrip() + "\" ")
